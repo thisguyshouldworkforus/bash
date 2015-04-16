b66ad26ed4ff771b05e38aa1c3ad2ac3a9a67230
Alexander Snyder <info@thisguyshouldworkfor.us>
2015-04-16 05:41:04 -0700

    updated headers per GPLv3

--- bash.headers.sh
+++ bash.headers.sh
@@ -1,4 +1,4 @@
-<?php
+﻿#!/bin/bash
 
 # --------------------------------------------------------------
 # Author: Alexander Snyder
@@ -27,24 +27,3 @@
 # Dependency: 
 # --------------------------------------------------------------
 
-// Create connection
-
-// GRANT ALL PRIVILEGES ON cphulkd.* TO brutescopy@'localhost' IDENTIFIED BY 'j3PZ89!fcAk2tM5mwD';
-
-$con = mysqli_connect("localhost","brutescopy","j3PZ89!fcAk2tM5mwD","cphulkd");
-
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-
-$moveip = ("INSERT INTO blacklist (IP) SELECT IP FROM brutes"); //move the IPs from the 'brutes' table onto the 'blacklist'
-$empty = ("TRUNCATE TABLE brutes"); //empty the brutes table
-
-mysqli_query($con, $moveip); // do it...
-mysqli_query($con, $empty); // get to da choppah!
-
-mysqli_close($con); // goooooo!
-
-?>
