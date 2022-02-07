f4ef8285c67cfe00e2b1ebac0df38d3700336f84
Alexander Snyder <alexander@sba.tc>
2022-02-07 09:16:08 -0700

    Removed old files we don't need anymore

--- grep-ip-from-config.sh
+++
@@ -1,1 +0,0 @@
-for i in $(grep -o --regexp="[0-9]\{1,3\}[\.][0-9]\{1,3\}[\.][0-9]\{1,3\}[\.][0-9]\{1,3\}" PSIC.mxtsessions); do sed "s/$i/$(dig -x $i | grep -o --regexp="[us].*[g][m][\.][c][o][m]")/g" -i PSIC.mxtsessions; done;
